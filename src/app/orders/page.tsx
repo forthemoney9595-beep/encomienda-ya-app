10fd89caa4c6a5eb1ff4e6afc6cb752b303ee2cd
David Lopez <95davidlopez.9595@gmail.com>
2025-09-29 22:34:06 +0000

    perfecto

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -39,7 +39,7 @@ export default async function OrdersPage() {
   }
 
   const userRole = user.role;
-  let orders = [];
+  let orders: any[] = [];
   let pageTitle = "";
   let pageDescription = "";
 
