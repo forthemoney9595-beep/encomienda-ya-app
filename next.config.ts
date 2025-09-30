af459fa0e4f62425b32adf44aa7f74d66fb4a787
David Lopez <95davidlopez.9595@gmail.com>
2025-09-30 00:46:02 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).     at cr

--- next.config.ts
+++ next.config.ts
@@ -30,14 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: 'AIzaSyDiFr0AGMK8v_dUnvTu_IhifSBfGBCNQm0',
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: 'studio-8973437171-6b185.firebaseapp.com',
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: 'studio-8973437171-6b185',
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: 'studio-8973437171-6b185.appspot.com',
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: '1091869412431',
-    NEXT_PUBLIC_FIREBASE_APP_ID: '1:1091869412431:web:e25c7cebb7b151423f2e3a',
-  },
 };
 
 export default nextConfig;
