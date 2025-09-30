d2c77e4065f2add5b9c698112f94b45ea6accd82
David Lopez <95davidlopez.9595@gmail.com>
2025-09-30 00:41:02 +0000

    perfecto, dime cuando hayas cambiado todo y deba probar el inicio

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration, now from environment variables
 const firebaseConfig = {
-  projectId: "studio-8973437171-6b185",
-  appId: "1:1091869412431:web:e25c7cebb7b151423f2e3a",
-  apiKey: "AIzaSyDiFr0AGMK8v_dUnvTu_IhifSBfGBCNQm0",
-  authDomain: "studio-8973437171-6b185.firebaseapp.com",
-  messagingSenderId: "1091869412431",
-  storageBucket: "studio-8973437171-6b185.appspot.com",
+  "projectId": "studio-8973437171-6b185",
+  "appId": "1:1091869412431:web:e25c7cebb7b151423f2e3a",
+  "apiKey": "AIzaSyDiFr0AGMK8v_dUnvTu_IhifSBfGBCNQm0",
+  "authDomain": "studio-8973437171-6b185.firebaseapp.com",
+  "messagingSenderId": "1091869412431",
+  "storageBucket": "studio-8973437171-6b185.appspot.com",
 };
 
 // Initialize Firebase for SSR
