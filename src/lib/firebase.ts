bf4755726ff9c8e631e3158c593d5b287e974598
David Lopez <95davidlopez.9595@gmail.com>
2025-09-30 00:47:34 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,16 @@
-// Import the functions you need from the SDKs you need
+"use client"
+
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration, now from environment variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: "studio-8973437171-6b185",
+  appId: "1:1091869412431:web:e25c7cebb7b151423f2e3a",
+  apiKey: "AIzaSyDiFr0AGMK8v_dUnvTu_IhifSBfGBCNQm0",
+  authDomain: "studio-8973437171-6b185.firebaseapp.com",
+  messagingSenderId: "1091869412431",
 };
 
 // Initialize Firebase for SSR
