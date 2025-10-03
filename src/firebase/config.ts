ca291149051387c85dd668ca98ea1197735de4fc
David Lopez <95davidlopez.9595@gmail.com>
2025-10-03 00:04:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8973437171-6b185",
+  "appId": "1:1091869412431:web:e25c7cebb7b151423f2e3a",
+  "apiKey": "AIzaSyDiFr0AGMK8v_dUnvTu_IhifSBfGBCNQm0",
+  "authDomain": "studio-8973437171-6b185.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1091869412431"
+};
