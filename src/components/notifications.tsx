528e5d63b0cf2fd781e33831d43bea4893929648
David Lopez <95davidlopez.9595@gmail.com>
2025-10-03 19:20:52 +0000

    otro pequeño detallito que note es que las notificaciones no tenian un s

--- src/components/notifications.tsx
+++ src/components/notifications.tsx
@@ -13,6 +13,7 @@ import { Button } from '@/components/ui/button';
 import { Bell, BellRing } from 'lucide-react';
 import { Badge } from './ui/badge';
 import { usePrototypeData } from '@/context/prototype-data-context';
+import { ScrollArea } from './ui/scroll-area';
 
 export function Notifications() {
   const { prototypeNotifications, clearPrototypeNotifications } = usePrototypeData();
@@ -33,20 +34,22 @@ export function Notifications() {
       <DropdownMenuContent align="end" className="w-80">
         <DropdownMenuLabel>Notificaciones</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        {prototypeNotifications.length === 0 ? (
-          <DropdownMenuItem disabled>No hay notificaciones nuevas.</DropdownMenuItem>
-        ) : (
-          prototypeNotifications.map((notification) => (
-            <DropdownMenuItem key={notification.id} className="flex flex-col items-start gap-1 whitespace-normal">
-              <div className="flex items-center gap-2">
-                  <BellRing className="h-4 w-4 text-muted-foreground" />
-                  <p className='font-semibold'>{notification.title}</p>
-              </div>
-              <p className='pl-6 text-sm text-muted-foreground'>{notification.description}</p>
-              <p className='pl-6 text-xs text-muted-foreground/70'>{notification.date}</p>
-            </DropdownMenuItem>
-          ))
-        )}
+        <ScrollArea className="max-h-80">
+          {prototypeNotifications.length === 0 ? (
+            <DropdownMenuItem disabled>No hay notificaciones nuevas.</DropdownMenuItem>
+          ) : (
+            prototypeNotifications.map((notification) => (
+              <DropdownMenuItem key={notification.id} className="flex flex-col items-start gap-1 whitespace-normal">
+                <div className="flex items-center gap-2">
+                    <BellRing className="h-4 w-4 text-muted-foreground" />
+                    <p className='font-semibold'>{notification.title}</p>
+                </div>
+                <p className='pl-6 text-sm text-muted-foreground'>{notification.description}</p>
+                <p className='pl-6 text-xs text-muted-foreground/70'>{notification.date}</p>
+              </DropdownMenuItem>
+            ))
+          )}
+        </ScrollArea>
          <DropdownMenuSeparator />
          <DropdownMenuItem onClick={clearPrototypeNotifications} className='justify-center text-sm text-muted-foreground hover:cursor-pointer'>
             Marcar todas como leídas
