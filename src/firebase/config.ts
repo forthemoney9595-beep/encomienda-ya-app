d210df7c3af598e2557486744a76aff23a826d74
David Lopez <95davidlopez.9595@gmail.com>
2025-10-14 20:41:01 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "king-12801",
-  "appId": "1:552356599343:web:e2b3e8a4a58928c292b3c2",
+  "projectId": "studio-354048519-4bc1e",
+  "appId": "1:933495393167:web:8397125928886576924845",
   "apiKey": "AIzaSyAz9gJt5-t9b6w7m5x4y3z2a1b0c9d8e7f",
-  "authDomain": "king-12801.firebaseapp.com",
+  "authDomain": "studio-354048519-4bc1e.firebaseapp.com",
   "measurementId": "G-MEASUREMENT_ID",
-  "messagingSenderId": "552356599343",
-  "storageBucket": "king-12801.appspot.com"
+  "messagingSenderId": "933495393167",
+  "storageBucket": "studio-354048519-4bc1e.appspot.com"
 };
