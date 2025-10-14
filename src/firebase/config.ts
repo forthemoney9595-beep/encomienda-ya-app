b066f12f4a7830ff8335a3568fe4bb5d00d0c7c8
David Lopez <95davidlopez.9595@gmail.com>
2025-10-14 20:48:31 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-354048519-4bc1e",
-  "appId": "1:933495393167:web:8397125928886576924845",
-  "apiKey": "AIzaSyAz9gJt5-t9b6w7m5x4y3z2a1b0c9d8e7f",
+  "appId": "1:230155353645:web:694381d43e63e9787a82a4",
+  "apiKey": "AIzaSyCsVGkQCAbEZ2gpdoEalomgee6XfUoN7Kg",
   "authDomain": "studio-354048519-4bc1e.firebaseapp.com",
-  "measurementId": "G-MEASUREMENT_ID",
-  "messagingSenderId": "933495393167",
-  "storageBucket": "studio-354048519-4bc1e.appspot.com"
+  "measurementId": "",
+  "messagingSenderId": "230155353645"
 };
